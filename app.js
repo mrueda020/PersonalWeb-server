5654c9737a25cfddd5dc2de6872e7cd5d7980801
Miguel Rueda <mrueda020@hotmail.com>
2021-02-10 17:56:53 -0600

    add user sign-up route

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const bodyParser = require("body-parser");
 const app = express();
 const { API_VERSION } = require("./config");
 
+const userRoutes = require("./routers/user");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(`/api/${API_VERSION}`, userRoutes);
 
 module.exports = app;
